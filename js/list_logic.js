30f81417ad844a36c57d336bd72066e06e3a3f4c
Ashley <ashleyjvelasquez@outlook.com>
2020-08-16 21:49:29 -0700

    adding more js functionality to to-do list

--- js/list_logic.js
+++ js/list_logic.js
@@ -2,8 +2,20 @@
 
 var list = document.getElementById('to-do-list');
 
-//      Add/Create logic
 
+//      Add/Create logic
+    function addItem(){
+        var item = document.createElement('p');
+        //user input box
+        var userInput = "1. Hello, this works"
+
+        //creates element with that item
+        
+        //TODO if null => " "(or does adding nothing make more sense?) else "user input"
+        list.appendChild(item);
+        item.innerText= userInput;
+    }
+    console.log(list);
 
 //      Delete logic
 
