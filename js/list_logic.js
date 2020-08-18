648492827e7c7aefffc33bae16845018dd2b647f
Ashley <ashleyjvelasquez@outlook.com>
2020-08-17 20:33:16 -0700

    Add button!

--- js/list_logic.js
+++ js/list_logic.js
@@ -1,21 +1,44 @@
 //      to-do list functions
 
 var list = document.getElementById('to-do-list');
-
+var item ="";
 
 //      Add/Create logic
-    function addItem(){
-        var item = document.createElement('p');
-        //user input box
-        var userInput = "1. Hello, this works"
-
-        //creates element with that item
-        
-        //TODO if null => " "(or does adding nothing make more sense?) else "user input"
-        list.appendChild(item);
-        item.innerText= userInput;
+    function removeUserInput(){
+        //getting rid of user inputs
+        var garbo = document.querySelector("form");
+        list.removeChild(garbo);
     }
-    console.log(list);
+
+    function addItem(value){
+        //adding item
+        var listItem=document.createElement('p');
+        list.appendChild(listItem);
+        listItem.innerText=value;
+    }
+
+    function askUserInput(){
+        var input = document.createElement('form');
+        input.innerHTML=
+        "<input type='text' name='to-do-item' id ='userItem' placeholder='Insert To Do Item' onchange='checkValue(this.value)'> <button type='button' onclick='getItem()'>Add to List</button>";
+
+        list.appendChild(input);
+    }
+
+    function getItem(){
+        if(item==null || item=="Insert To Do Item" ||item==""){
+            alert('No task added. Error: '+item);
+        }
+        else{
+            addItem(item);
+            removeUserInput();
+        }
+    }
+
+    function checkValue(value){
+            item=value;
+    }
+
 
 //      Delete logic
 
